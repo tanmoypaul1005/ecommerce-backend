c0097e56b9e17d31503711a79461f77b00896e29
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-14 22:22:45 +0600

    Add getAllProducts method to ProductService and implement corresponding endpoint in ProductController

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Get } from '@nestjs/common';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -7,13 +7,19 @@ import { CreateProductDto } from './dto/product.dto';
 
 @Controller('product')
 export class ProductController {
-    constructor(private readonly productService: ProductService) {}
+    constructor(private readonly productService: ProductService) { }
 
-     @Post('')
-     @UseGuards(JwtAuthGuard, RolesGuard)
-     @Roles('ADMIN', 'SUPER_ADMIN')
-     register(@Body() dto: CreateProductDto) {
-       return this.productService.createProduct(dto);
-     }
+    @Post('')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('ADMIN', 'SUPER_ADMIN')
+    register(@Body() dto: CreateProductDto) {
+        return this.productService.createProduct(dto);
+    }
+
+
+    @Get('')
+    getAllProducts() {
+        return this.productService.getAllProducts();
+    }
 
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -12,4 +12,9 @@ export class ProductService {
         })
         return product;
     }
+
+    async getAllProducts(){
+        const product =await this.prisma.product.findMany();
+        return product;
+    }
 }
