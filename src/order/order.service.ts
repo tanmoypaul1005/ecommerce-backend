bdec05c779430fca1ae2f59d016700371b114a39
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-18 23:11:37 +0600

    Implement OrderService with getAllOrders method for fetching user orders

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getAllOrders(params: { userId?: string }) {
+        const orders = await this.prisma.order.findMany({
+            where: {
+                userId: params.userId,
+            },
+            include: {
+                items: true,
+                address: true,
+                user: true,
+            },
+        });
+        return orders;
+    }
+}
