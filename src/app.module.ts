53ad3030b4809001d95bee93c81f8589c225ebc6
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-18 22:52:21 +0600

    Add Order module with controller and service for order management

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { AddressModule } from './address/address.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ProductModule,ProductModule, CategoryModule, AddressModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ProductModule,ProductModule, CategoryModule, AddressModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
