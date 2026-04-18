f7e7e404bca44d85db047fe0799693e4a1387b8c
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-18 23:37:20 +0600

    Implement OrderController with getAllOrders method for fetching orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private readonly orderService: OrderService) { }
+
+    @Get('')
+    getAllOrders(@Body() params: { userId?: string }) {
+        return this.orderService.getAllOrders(params);
+    }
+
+}
