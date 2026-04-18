3468b6e4dbf1db0fd43dcaf1e40fb58a2325834d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-18 16:30:16 +0600

    Add addresses field to User model and import AuthModule in AddressModule

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AddressController],
-  providers: [AddressService]
+  providers: [AddressService],
 })
 export class AddressModule {}
