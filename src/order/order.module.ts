a2ba77442a63448c6b8d46255c4deb1487698b7d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-19 22:29:27 +0600

    Add class-transformer dependency and integrate AuthModule in OrderModule

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [OrderController],
   providers: [OrderService]
 })
