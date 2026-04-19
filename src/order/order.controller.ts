b5d991ea63dedbd628d29efa87ad31d536c1a274
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-19 22:20:17 +0600

    Add createOrder method to OrderController with JWT authentication

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { OrderService } from './order.service';
 
@@ -14,6 +14,16 @@ export class OrderController {
             throw new UnauthorizedException('Missing user id');
         }
         return this.orderService.getAllOrders(userId);
+    }
+
+    @Post('')
+    @UseGuards(JwtAuthGuard)
+    createOrder(@Req() req: { user?: { sub?: string } }, paymentMethod, items: { productId: string; quantity: number }[], addressId: string) {
+        const userId = req.user?.sub;
+        if (!userId) {
+            throw new UnauthorizedException('Missing user id');
+        }
+        return this.orderService.createOrder(userId, paymentMethod, items, addressId);
     }
 
 }
