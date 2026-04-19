024a44540717bf18d429d457e8bba0e239d24fdc
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-04-19 22:18:43 +0600

    Add processing status to orders and update payment status logic in createOrder method

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -18,4 +18,23 @@ export class OrderService {
         });
         return orders;
     }
+
+    async createOrder(userId: string, paymentMethod,items: { productId: string; quantity: number }[], addressId: string) {
+        const order = await this.prisma.order.create({
+            data: {
+                userId,
+                addressId,
+                items,
+                paymentMethod,
+                status: (paymentMethod === 'CARD'  || paymentMethod === 'BKASH') ? 'PROCESSING' : 'PENDING',
+                paymentStatus: (paymentMethod === 'CARD'  || paymentMethod === 'BKASH') ? 'PAID' : 'PENDING',
+            },
+            include: {
+                items: true,    
+                address: true,
+                user: true,
+            },
+        });
+        return order;
+    }
 }
