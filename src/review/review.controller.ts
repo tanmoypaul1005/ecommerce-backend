fdd8792f714937fad7adf8bdd7f9625b528d8f51
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-02 21:46:15 +0600

    Add createReview method to ReviewService and implement createReview endpoint in ReviewController

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ReviewService } from './review.service';
 
 @Controller('review')
@@ -9,5 +9,9 @@ export class ReviewController {
     async getAllReviews() {
         return await this.reviewService.getAllReviews();
     }
-}
- ;
+
+    @Post()
+    async createReview() {
+        return await this.reviewService.createReview();
+    }
+};
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -5,6 +5,17 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ReviewService {
     constructor(private readonly prisma: PrismaService) { }
 
+    async createReview() {
+        await this.prisma.review.create({
+            data: {
+                rating: 5,
+                comment: 'Great product!',
+                productId: 'some-product-id',
+                userId: 'some-user-id',
+            }
+        });
+    }
+
     async getAllReviews() {
         const reviews = await this.prisma.review.findMany();
         return reviews;
