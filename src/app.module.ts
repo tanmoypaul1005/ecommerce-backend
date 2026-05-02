c10080f21d1cb652c60964a6364862f753b85927
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-02 17:50:28 +0600

    Add Review module, controller, and service to handle product reviews

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { CategoryModule } from './category/category.module';
 import { AddressModule } from './address/address.module';
 import { OrderModule } from './order/order.module';
 import { UploadModule } from './upload/upload.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ProductModule,ProductModule, CategoryModule, AddressModule, OrderModule, UploadModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ProductModule,ProductModule, CategoryModule, AddressModule, OrderModule, UploadModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
