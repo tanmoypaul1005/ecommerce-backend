2383abe9d5b8ebacdcea0d8603e6c57a002c492a
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-03 23:14:46 +0600

    Implement BrandController and BrandService with getAllBrands method for brand retrieval

--- src/brand/brand.controller.ts
+++ src/brand/brand.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BrandService } from './brand.service';
 
 @Controller('brand')
-export class BrandController {}
+export class BrandController {
+    constructor(private readonly brandService: BrandService) { }
+
+    @Get()
+    async getAllBrands() {
+        return await this.brandService.getAllBrands();
+    }
+
+ }
--- src/brand/brand.service.ts
+++ src/brand/brand.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class BrandService {}
+export class BrandService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAllBrands() {
+        const brands = await this.prisma.brand.findMany();
+        return brands;
+    }
+}
