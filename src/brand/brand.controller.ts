0bfb011f68d654d2e5a01f2edd34a86c980687b0
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-05-04 20:48:34 +0600

    Add createBrand method to BrandService and corresponding endpoint in BrandController

--- src/brand/brand.controller.ts
+++ src/brand/brand.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BrandService } from './brand.service';
+import { BrandDto } from './dto/brand.dto';
 
 @Controller('brand')
 export class BrandController {
@@ -10,4 +11,9 @@ export class BrandController {
         return await this.brandService.getAllBrands();
     }
 
- }
+    @Post()
+    async createBrand(@Body() dto: BrandDto) {
+        return await this.brandService.createBrand(dto);
+    }
+
+}
--- src/brand/brand.service.ts
+++ src/brand/brand.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { BrandDto } from './dto/brand.dto';
 
 @Injectable()
 export class BrandService {
@@ -8,5 +9,15 @@ export class BrandService {
     async getAllBrands() {
         const brands = await this.prisma.brand.findMany();
         return brands;
+    }
+
+    async createBrand(dto:BrandDto) {
+        const created = await this.prisma.brand.create({
+            data: dto,
+        });
+        return {
+            message: 'Brand created',
+            data: created,
+        };
     }
 }
